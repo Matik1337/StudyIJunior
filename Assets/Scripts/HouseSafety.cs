3a8d14419b76621e1d66c603942e9f9e162c4742
Matik1337 <53432275+Matik1337@users.noreply.github.com>
2021-02-02 16:33:23 +0300

    SignalingTask

---
+++ Assets/Scripts/HouseSafety.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseSafety : MonoBehaviour
+{
+    private SignalingControl _signalingControl;
+    private bool _isSignalPlaying;
+    private void Start()
+    {
+        _signalingControl = gameObject.GetComponentInChildren<SignalingControl>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            _isSignalPlaying = true;
+            _signalingControl.SwichSignalState(_isSignalPlaying);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            _isSignalPlaying = false;
+            _signalingControl.SwichSignalState(_isSignalPlaying);        
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _rb.velocity = Vector3.left;
+    }
+}
